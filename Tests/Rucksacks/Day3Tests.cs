51369ab8ccfa593cb289fef9e34c9bd702049299
Arthur Yvrard <arthur.yvrard@gmail.com>
2022-12-03 12:40:56 +0100

    oops: misnamed test method (yes, I use copy-paste to generate tests)

--- Tests/Rucksacks/Day3Tests.cs
+++ Tests/Rucksacks/Day3Tests.cs
@@ -5,6 +5,14 @@ namespace Tests.Rucksacks
     public class Day3Tests
     {
         [Fact]
+        public void ShouldCalculateTotalPrioritiesOfCommonInRucksacks()
+        {
+            var analyser = new RucksackAnalyser(Day3MockData.Test);
+
+            Assert.Equal(157, analyser.SumTotalPriorities());
+        }
+
+        [Fact]
         public void ShouldCalculateHighestCaloriesAvailable()
         {
             var analyser = new RucksackAnalyser(Day3MockData.Test);
