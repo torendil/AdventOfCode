3e25187771660814baf1d9bdbc2234ca1630c4d9
Arthur Yvrard <arthur.yvrard@gmail.com>
2022-12-08 12:14:24 +0100

    Day 6 part 1 (COVID hit me, I'm late)

---
+++ AdventOfCode/CommunicationDevice/SignalProcessor.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode
+{
+    public class SignalProcessor
+    {
+        private const int bufferSize = 4;
+        private readonly string feed;
+
+        public SignalProcessor(string feed)
+        {
+            this.feed = feed;
+        }
+
+        public int GetMarkerLocation()
+        {
+            using var reader = new StringReader(feed);
+
+            var buffer = new Queue<char>(bufferSize + 1);
+            int currentCharAsInt;
+
+            int position = 0;
+
+            while ((currentCharAsInt = reader.Read()) != -1)
+            {
+                position++;
+                ChangeBuffer(buffer, currentCharAsInt);
+                
+                if (buffer.Distinct().Count() == 4)
+                {
+                    return position;
+                }
+            }
+
+            return -1;
+        }
+
+        private static void ChangeBuffer(Queue<char> buffer, int currentCharAsInt)
+        {
+            buffer.Enqueue((char)currentCharAsInt);
+            if (buffer.Count > bufferSize)
+            {
+                buffer.Dequeue();
+            }
+        }
+    }
+}
---
+++ Tests/CommunicationDevice/Day6Tests.cs
@@ -0,0 +1,20 @@
+using AdventOfCode;
+using AdventOfCode.MovingCrates;
+
+namespace Tests.CommunicationDevice
+{
+    public class Day6Tests
+    {
+        [Theory]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void ShouldFindMarker(string feed, int expectedLocation)
+        {
+            var signalProcessor = new SignalProcessor(feed);
+
+            Assert.Equal(expectedLocation, signalProcessor.GetMarkerLocation());
+        }
+    }
+}
