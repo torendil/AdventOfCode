513da2056c47e0f643ab4b33c9a80800e91f3e9d
Arthur Yvrard <arthur.yvrard@gmail.com>
2022-12-08 20:05:49 +0100

    After refactoring

--- AdventOfCode/CommunicationDevice/FileSystemAnalyser.cs
+++ AdventOfCode/CommunicationDevice/FileSystemAnalyser.cs
@@ -2,87 +2,17 @@
 {
     public class FileSystemAnalyser
     {
-        private readonly Folder root = new("/");
+        private readonly Folder root;
 
         public FileSystemAnalyser(string consoleOutput)
         {
-            using var reader = new StringReader(consoleOutput);
-
-            // manage root
-            string? line = reader.ReadLine();
-            Folder current = root;
-
-            while ((line = reader.ReadLine()) != null)
-            {
-                if (line.StartsWith("$ cd .."))
-                {
-                    if(current.Parent == null)
-                    {
-                        throw new Exception("Should never go higher than root");
-                    }
-                    current = current.Parent;
-                }
-                else
-                {
-                    if (line.StartsWith("$ cd"))
-                    {
-                        var folderName = line["$ cd ".Length..];
-                        current = current.SubFolders.First(sub => sub.Name == folderName);
-                    }
-
-                    if (line.StartsWith("$ ls"))
-                    {
-                        ReadFolderContent(current, reader);
-                    }
-                }
-            }
-        }
-
-        private static void ReadFolderContent(Folder current, StringReader reader)
-        {
-            while (reader.Peek() != '$' && reader.Peek() != -1)
-            {
-                string? line = reader.ReadLine();
-                if (line == null)
-                {
-                    throw new Exception("Line should never be null here");
-                }
-                if (line.StartsWith("dir "))
-                {
-                    current.SubFolders.Add(new Folder(line["dir ".Length..], current));
-                }
-                else
-                {
-                    var fileDef = line.Split(' ');
-                    current.Files.Add(new DeviceFile(fileDef[1], int.Parse(fileDef[0])));
-                }
-            }
-        }
-
-        private Folder? FindSubDir(Folder currentFolder, string folderName)
-        {
-            if (currentFolder.Name == folderName)
-            {
-                return currentFolder;
-            }
-
-            foreach (var sub in currentFolder.SubFolders)
-            {
-                var result = FindSubDir(sub, folderName);
-                if (result != null)
-                {
-                    return result;
-                }
-            }
-            return null;
+            var parser = new FileSystemParser(consoleOutput);
+            root = parser.Root;
         }
 
         public IEnumerable<Folder> GetFoldersUnder100k(Folder? current = null)
         {
-            if(current == null)
-            {
-                current = root;
-            }
+            current ??= root;
             var result = current.SubFolders.Where(sub => sub.Size < 100000);
             foreach(var sub in current.SubFolders)
             {
---
+++ AdventOfCode/CommunicationDevice/FileSystemParserParser.cs
@@ -0,0 +1,77 @@
+﻿namespace AdventOfCode.CommunicationDevice
+{
+    internal class FileSystemParser
+    {
+        internal Folder Root { get; } = new("/");
+
+        public FileSystemParser(string input)
+        {
+            Parse(input);
+        }
+
+        public void Parse(string input)
+        {
+            using var reader = new StringReader(input);
+
+            // manage root
+            string? line = reader.ReadLine();
+            Folder current = Root;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.StartsWith("$ cd .."))
+                {
+                    current = GetParent(current);
+                }
+                else
+                {
+                    if (line.StartsWith("$ cd"))
+                    {
+                        current = AccessChildThroughCommand(line, current);
+                    }
+
+                    if (line.StartsWith("$ ls"))
+                    {
+                        ReadFolderContent(current, reader);
+                    }
+                }
+            }
+        }
+
+        private static Folder GetParent(Folder current)
+        {
+            if (current.Parent == null)
+            {
+                throw new Exception("Should never go higher than root");
+            }
+            return current.Parent;
+        }
+
+        private static Folder AccessChildThroughCommand(string line, Folder current)
+        {
+            var folderName = line["$ cd ".Length..];
+            return current.SubFolders.First(sub => sub.Name == folderName);
+        }
+
+        private static void ReadFolderContent(Folder current, StringReader reader)
+        {
+            while (reader.Peek() != '$' && reader.Peek() != -1)
+            {
+                string? line = reader.ReadLine();
+                if (line == null)
+                {
+                    throw new Exception("Line should never be null here");
+                }
+                if (line.StartsWith("dir "))
+                {
+                    current.SubFolders.Add(new Folder(line["dir ".Length..], current));
+                }
+                else
+                {
+                    var fileDef = line.Split(' ');
+                    current.Files.Add(new DeviceFile(fileDef[1], int.Parse(fileDef[0])));
+                }
+            }
+        }
+    }
+}
